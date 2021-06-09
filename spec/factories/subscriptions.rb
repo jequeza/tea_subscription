0f3f6770caad251770423f09604a0710476ab757
Jesus Quezada-Guillen <maj3st1@gmail.com>
2021-06-08 20:31:37 -0500

    Update factories with Faker

--- spec/factories/subscriptions.rb
+++ spec/factories/subscriptions.rb
@@ -1,9 +1,9 @@
 FactoryBot.define do
   factory :subscription do
     customer { nil }
-    title { "MyString" }
-    price { 1 }
-    status { 1 }
-    frequency { 1 }
+    title { Faker::Subscription.plan }
+    price { Faker::Commerce.price(range: 0..30.0) }
+    status { Faker::Number.between(from: 0, to: 1)  }
+    frequency { Faker::Number.between(from: 0, to: 2)  }
   end
 end
--- spec/factories/teas.rb
+++ spec/factories/teas.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :tea do
-    title { "MyString" }
-    description { "MyString" }
-    temperature { 1 }
-    brew_time { 1 }
+    title { Faker::Tea.variety }
+    description { Faker::Movie.quote }
+    temperature { Faker::Number.between(from: 80, to: 100) }
+    brew_time { Faker::Number.between(from: 1, to: 12) }
   end
 end
