11d460dc4902880109c2f402d6606fae4ae7860a
Jesus Quezada-Guillen <maj3st1@gmail.com>
2021-06-10 11:51:16 -0500

    Add subscription controller along with action and error handling.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::API
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+  rescue_from ActiveRecord::RecordNotFound, with: :render_unprocessable_entity_response
+
+  def render_unprocessable_entity_response(exception)
+    render json: {error: exception.message}, status: 400
+  end
+
+  def invalid_params
+    render json: {error: 'invalid user'}, status: :bad_request
+  end
 end
